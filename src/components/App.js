b8b69c22881f7b9b657de21d999f16d0890b7138
meshamanth <shamanth.kr@gmail.com>
2021-06-08 19:03:04 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+          <h2>Hima Das</h2>
+          <img
+            src="https://im.rediff.com/sports/2018/apr/10hima.jpg?w=300&h=400"
+            alt="avatar_img"
+          />
+        </div>
+        <div className="bottom">
+          <p>+123 456 789</p>
+          <p>himadas@india.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
